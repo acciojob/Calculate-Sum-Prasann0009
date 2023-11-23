5ddc095c3ef814ad4b5c85cec6a2067ef35759b6
Prasann0009 <130238044+Prasann0009@users.noreply.github.com>
2023-11-23 21:52:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function printsum()
+{
+const n1=parseInt(document.getElementById("n1").value);
+const n2=parseInt(document.getElementById("n2").value);
+
+let sum = n1+n2;
+   
+    document.getElementById("sum").innerText = sum;
+}
